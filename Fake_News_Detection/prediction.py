3d991bddea985039556d617611a0385e6da16996
Kunal <kunal.sonawane@somaiya.edu>
2018-10-20 23:44:12 +0530

    got output from model

--- Fake_News_Detection/prediction.py
+++ Fake_News_Detection/prediction.py
@@ -1,20 +1,41 @@
 # -*- coding: utf-8 -*-
-
-import pickle
-
-# var = input("Please enter the news text you want to verify: ")
-# print("You entered: " + str(var))
-
-
+import pickle, os, json
+import pyphi
 #function to run for prediction
 def detecting_fake_news(var):
-#retrieving the best model for prediction call
-    load_model = pickle.load(open('final_model.sav', 'rb'))
+#retrieving the best model for prediction
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    load_model = pickle.load(open(PROJECT_ROOT+'/final_model.sav', 'rb'))
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
+    print("The given statement is ",prediction[0])
+    print("The truth probability score is ",prob[0][1])
+    return prediction[0], prob[0][1]
+# var = input("Please enter the news text you want to verify: ")
+# print("You entered: " + str(var))
 
-if __name__ == '__main__':
-    detecting_fake_news(var)
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = PROJECT_ROOT[0: -19]
+something = dict()
+with open(PROJECT_ROOT + 'app/scraping/data/scraped_articles.json', 'r') as file:
+    data = json.load(file)
+    print(data)
+    for article in data['articles']:
+        verdict, prob = detecting_fake_news(article['text'])
+        if verdict == True:
+            verdict = 'True'
+        else:
+            verdict = 'False'
+        article['verdict'] = verdict
+        article['probability'] = prob
+        print(verdict, str(prob))
+    print(data)
+    something = data
+    print(type(something))
+    print(something)
+# with open(PROJECT_ROOT + 'app/scraping/data/scraped_articles.json', 'w') as file:
+#     json.dump(data, file, indent = 4, sort_keys = True)
+with open('app/scraping/data/scraped_articles.json', 'w') as fp:
+    json.dump(something, fp, indent = 4, sort_keys = True)
+    print('Fuck Yeah!')
