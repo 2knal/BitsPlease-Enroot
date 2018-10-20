828f995dd154ae438c4f4599919d7c9a42a87ddc
Kunal <kunal.sonawane@somaiya.edu>
2018-10-20 20:01:42 +0530

    scraped news and can pass it to model

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os, subprocess
 
 def home(request):
+    output = script(request)
+    ml = train(request)
     return render(request, 'app/home.html')
+
+def script(request):
+    if request.method == 'GET':
+        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+        print(PROJECT_ROOT)
+        return subprocess.run(['python', PROJECT_ROOT+'/scraping/NewsScraper.py'])
+def train(request):
+    if request.method == 'GET':
+        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+        print('00'+PROJECT_ROOT)
+        PROJECT_ROOT = PROJECT_ROOT[0: -4]
+        print('01'+PROJECT_ROOT)
+        return subprocess.run(['python', PROJECT_ROOT+'/Fake_News_Detection/prediction.py'])
